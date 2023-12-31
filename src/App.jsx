b76fad953a56abb6c376b3f7aad864b66c33bfcb
“itsMohammedNayeem” <Mohammed.Nayeem@weareplanet.com>
2023-12-31 23:59:01 +0530

    Update title and add fun animated card

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
 function App() {
-  return <h1 className="">App</h1>;
+  return (
+    <div className="card text-neutral-100 bg-[url('/img/macbookPic.png')] bg-cover pt-40 max-w-[35ch] rounded-lg overflow-hidden">
+      <div className="card-content p-[1.5rem] bg-gradient-to-b from-firstGrad via-secondGrad via-20% to-lastGrad">
+        <h2 className="card-title text-2xl font-bold relative mb-5 w-max outline-red after:content-[''] after:left-[calc(-1.5rem)] after:bottom-0 after:absolute after:h-1 after:bg-accent-400 after:w-[calc(100%_+_1.5rem)]">
+          Something awesome
+        </h2>
+        <p className="card-body mb-5">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugiat rem
+          facilis.
+        </p>
+        <a
+          href="#"
+          className="button cursor-pointer inline-block no-underline text-buttonColor bg-accent-400 py-[0.5em] px-[1.25em] hover:bg-neutral-100 focus:bg-neutral-100"
+        >
+          LEARN MORE
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // black: "#000000",
+        neutral: {
+          100: "#ffffff",
+          900: "#13171b",
+        },
+        accent: {
+          400: "#42f584",
+        },
+        firstGrad: "hsl(0 0% 0% / 0)",
+        secondGrad: "hsl(20 0% 0% / 0.3)",
+        lastGrad: "hsl(0 0% 0% / 1)",
+        buttonColor: "hsl(207 19% 9%)",
+      },
+    },
   },
   plugins: [],
 };
